c1aaf0717455ecd9cc3a3c33407fbbe2aec1070a
Dark <nvngoca10@gmail.com>
2019-10-03 10:38:12 +0700

    Add project files.

---
+++ AppOOP/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppOOP
+{
+    class Student
+    {
+        private string Name;
+
+        public string StudentName
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+
+        public Student(string stName)
+        {
+            this.Name = stName;
+        }
+
+        public Student()
+        {
+        }
+
+        public virtual void Talk()
+        {
+            if (StudentName.Equals("Minh"))
+                System.Windows.MessageBox.Show("My name is " + this.StudentName + " i am a college student");
+            else if (StudentName.Equals("Dung"))
+                System.Windows.MessageBox.Show("My name is " + this.StudentName + " i am a university student");
+        }
+    }
+}
---
+++ AppOOP/StudentCollege.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppOOP
+{
+    class StudentCollege : Student
+    {
+        public override void Talk()
+        {
+            base.Talk();
+        }
+    }
+}
